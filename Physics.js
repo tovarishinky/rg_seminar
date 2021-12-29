2d93fe163aa155ff462d38a8ded6e664846ea68f
Tevž Basaj <76099047+Tevz-B@users.noreply.github.com>
2021-12-29 12:46:07 +0100

    Fixed movement bug, fixed collision bug
    collision rab odstranjenje rotacij

---
+++ Physics.js
@@ -0,0 +1,118 @@
+import { vec3, mat4 } from '../../lib/gl-matrix-module.js';
+
+export class Physics {
+
+    constructor(scene) {
+        this.scene = scene;
+        this.falling = false;
+    }
+
+    update(dt, player) {
+        this.falling = true; // set falling, set to false in collision detection if necessary
+        vec3.scaleAndAdd(player.translation, player.translation, player.velocity, dt);
+        player.updateMatrix();
+        this.scene.traverse(node => {
+            if (node.mesh && player.velocity && player.feet) {
+                this.resolveCollision(player, node);
+            }
+        });
+        this.updateFalling(player);
+    }
+
+    updateFalling(player) {
+        player.falling = this.falling;
+    }
+
+    intervalIntersection(min1, max1, min2, max2) {
+        return !(min1 > max2 || min2 > max1);
+    }
+
+    aabbIntersection(aabb1, aabb2) {
+        return this.intervalIntersection(aabb1.min[0], aabb1.max[0], aabb2.min[0], aabb2.max[0])
+            && this.intervalIntersection(aabb1.min[1], aabb1.max[1], aabb2.min[1], aabb2.max[1])
+            && this.intervalIntersection(aabb1.min[2], aabb1.max[2], aabb2.min[2], aabb2.max[2]);
+    }
+
+    resolveCollision(a, b) {
+        // Update bounding boxes with global translation.
+        const ta = a.getGlobalTransform();
+        const tb = b.getGlobalTransform();
+
+        const posa = mat4.getTranslation(vec3.create(), ta);
+        const posb = mat4.getTranslation(vec3.create(), tb);
+        
+        const mina = vec3.add(vec3.create(), posa, a.collisionMin);
+        const maxa = vec3.add(vec3.create(), posa, a.collisionMax);
+
+        let mb = vec3.clone(b.mesh.primitives[0].attributes.POSITION.min);
+        let mbb = vec3.clone(b.mesh.primitives[0].attributes.POSITION.max);
+
+        const minb = vec3.add(vec3.create(), posb, mb);
+        const maxb = vec3.add(vec3.create(), posb, mbb);
+        // feet
+        const minaf = vec3.add(vec3.create(), posa, a.feet.min);
+        const maxaf = vec3.add(vec3.create(), posa, a.feet.max);
+
+        // Check if there is collision.
+        const isColliding = this.aabbIntersection({
+            min: mina,
+            max: maxa
+        }, {
+            min: minb,
+            max: maxb
+        });
+
+        // check for ground contact
+        const isCollidingFeet = this.aabbIntersection({
+            min: minaf,
+            max: maxaf
+        }, {
+            min: minb,
+            max: maxb
+        });
+
+        // set falling
+        if (isCollidingFeet) {
+            this.falling = false;
+        }
+
+        if (!isColliding) {
+            return;
+        }
+
+        // Move node A minimally to avoid collision.
+        const diffa = vec3.sub(vec3.create(), maxb, mina);
+        const diffb = vec3.sub(vec3.create(), maxa, minb);
+
+        let minDiff = Infinity;
+        let minDirection = [0, 0, 0];
+        if (diffa[0] >= 0 && diffa[0] < minDiff) {
+            minDiff = diffa[0];
+            minDirection = [minDiff, 0, 0];
+        }
+        if (diffa[1] >= 0 && diffa[1] < minDiff) {
+            minDiff = diffa[1];
+            minDirection = [0, minDiff, 0];
+        }
+        if (diffa[2] >= 0 && diffa[2] < minDiff) {
+            minDiff = diffa[2];
+            minDirection = [0, 0, minDiff];
+        }
+        if (diffb[0] >= 0 && diffb[0] < minDiff) {
+            minDiff = diffb[0];
+            minDirection = [-minDiff, 0, 0];
+        }
+        if (diffb[1] >= 0 && diffb[1] < minDiff) {
+            minDiff = diffb[1];
+            minDirection = [0, -minDiff, 0];
+        }
+        if (diffb[2] >= 0 && diffb[2] < minDiff) {
+            minDiff = diffb[2];
+            minDirection = [0, 0, -minDiff];
+        }
+
+        vec3.add(a.translation, a.translation, minDirection);
+        a.updateMatrix();
+    }
+
+}
---
+++ Player.js
@@ -0,0 +1,198 @@
+import { mat4, quat, vec3 } from "../../lib/gl-matrix-module.js";
+import { Node } from "./Node.js";
+
+export class Player extends Node {
+    constructor(options = {}) {
+        super(options);
+
+        this.dims = {width: 0.6, height: 2.5, length: 0.6}; // set collision box for player
+        this.gravity = -5;    // set gravity
+        this.jumpHeight = 12;    // set Jump
+        this.walkSpeed = 4;      // set max walking speed
+        this.sprintSpeed = 6;   // set max sprinting speed
+        this.mvAcc = 1;         // set move acceleration TODO not implemeted yet
+        this.acceleration = 5; // set acceleration
+        this.maxSpeed = this.walkSpeed;
+
+        this.rotation = vec3.fromValues(0,0,0); // Euler rotation  instead quat
+        this.mousemoveHandler = this.mousemoveHandler.bind(this);
+        this.keydownHandler = this.keydownHandler.bind(this);
+        this.keyupHandler = this.keyupHandler.bind(this);
+        this.keys = {};
+        this.falling = false;
+        this.waitForJump = false;
+        this.sprint = false;
+        this.player = null;
+        
+        this.autoJump = false;
+        this.velocity = [0, 0, 0];
+        this.mouseSensitivity = 0.002;
+        this.friction = 0.2;
+        this.collisionMin = [-this.dims.width/2, -this.dims.height, -this.dims.length/2]; // collisionbox
+        this.collisionMax = [this.dims.width/2, 0.2, this.dims.length/2]; // collisionbox
+        this.feet = {
+            min: [
+                this.collisionMin[0] + 0.1, 
+                this.collisionMin[1] - 0.1, 
+                this.collisionMin[2] + 0.1],
+            max: [
+                this.collisionMax[0] - 0.1, 
+                this.collisionMax[1] + 0.1, 
+                this.collisionMax[2] - 0.1]
+        };
+
+        setInterval(() => {console.log(
+            'x: ', Math.round(this.translation[0]).toString(), 
+            '\ny: ', Math.round(this.translation[1]).toString(), 
+            '\nz: ', Math.round(this.translation[2]).toString(),
+            '\nspeed: ', this.velocity)}, 500); // TODO remove - debug
+    }
+
+    setFeet(ft) {
+        this.feet = ft;
+        console.log(this.feet);
+    }
+
+    getPlayer() {
+        this.player = this.children[0];
+    }
+
+    updateProjection() {
+        mat4.perspective(this.projection, this.fov, this.aspect, this.near, this.far);
+    }
+
+    update(dt) {
+        const c = this;
+        // TODO falling boolean
+
+
+        const forward = vec3.set(vec3.create(),
+            -Math.sin(c.rotation[1]), 0, -Math.cos(c.rotation[1]));
+        const right = vec3.set(vec3.create(),
+            Math.cos(c.rotation[1]), 0, -Math.sin(c.rotation[1]));
+        const up = vec3.set(vec3.create(), 0, c.jumpHeight, 0);  // set jump height
+        const down = vec3.set(vec3.create(), 0, c.gravity, 0);
+
+        // 1: add movement acceleration
+        let acc = vec3.create();
+        if (this.keys['KeyW']) {
+            vec3.add(acc, acc, forward);
+        }
+        if (this.keys['KeyS']) {
+            vec3.sub(acc, acc, forward);
+        }
+        if (this.keys['KeyD']) {
+            vec3.add(acc, acc, right);
+        }
+        if (this.keys['KeyA']) {
+            vec3.sub(acc, acc, right);
+        }
+        if (this.keys['ShiftLeft']) {
+            this.sprint = true;
+            c.maxSpeed = c.sprintSpeed;
+        }
+        if (!this.keys['ShiftLeft']) {
+            this.sprint = false;
+            c.maxSpeed = c.walkSpeed;
+        }
+
+        // verti move
+        if (!this.falling && !this.waitForJump && this.keys['Space']) {
+            this.waitForJump = true;
+            vec3.set(c.velocity, c.velocity[0], up[1], c.velocity[2]); // use add for jump dependend on landing (if you jump after landing you get a penalty)
+            setTimeout(() => {this.waitForJump = false;}, 700); // wawit 0.7s for next jump
+        } 
+        if (this.falling) {
+            vec3.add(acc, acc, down);
+        }
+        //console.log(this.autoJump);
+        if (this.autoJump) {
+            //this.waitForJump = true;
+            vec3.set(c.velocity, c.velocity[0], up[1] * 0.2, c.velocity[2]);
+            this.autoJump = false;
+        }
+        
+
+
+        // 2: update velocity
+        vec3.scaleAndAdd(c.velocity, c.velocity, acc, dt * c.acceleration);
+
+        // 3: if no movement and on ground(falling = false), apply friction
+        if (!this.keys['KeyW'] &&
+            !this.keys['KeyS'] &&
+            !this.keys['KeyD'] &&
+            !this.keys['KeyA'] &&
+            !this.keys['Space'] &&
+            !this.falling) 
+        {   
+            vec3.scale(c.velocity, c.velocity, 1 - c.friction);
+        }
+        console.log(this.falling);
+        if (!this.falling) {
+            vec3.set(c.velocity, c.velocity[0], Math.max(c.velocity[1], -1), c.velocity[2]);
+        }
+
+        // 4: limit horizontal speed
+        const len = vec3.len(vec3.set(vec3.create(), c.velocity[0], 0, c.velocity[2]));
+        if (len > c.maxSpeed) {
+            vec3.mul(c.velocity, c.velocity, vec3.set(vec3.create(), c.maxSpeed / len, 1, c.maxSpeed / len));
+        }
+    }
+
+    enableCam() {
+        document.addEventListener('mousemove', this.mousemoveHandler);
+        document.addEventListener('keydown', this.keydownHandler);
+        document.addEventListener('keyup', this.keyupHandler);
+    }
+
+    disableCam() {
+        document.removeEventListener('mousemove', this.mousemoveHandler);
+        document.removeEventListener('keydown', this.keydownHandler);
+        document.removeEventListener('keyup', this.keyupHandler);
+
+        for (let key in this.keys) {
+            this.keys[key] = false;
+        }
+    }
+
+    mousemoveHandler(e) {
+        const p = this;
+        const dx = e.movementX * p.mouseSensitivity;
+        const dy = e.movementY * p.mouseSensitivity;
+
+        p.rotation[0] -= dy;
+        p.rotation[1] -= dx;
+        //c.player.rotation[0] += dy * c.mouseSensitivity;  // dont turn player (unturn) if you look up, only about Y
+
+        const pi = Math.PI;
+        const twopi = pi * 2;
+        const halfpi = pi / 2;
+        if (p.rotation[0] > halfpi) {
+            p.rotation[0] = halfpi;
+        }
+        if (p.rotation[0] < -halfpi) {
+            p.rotation[0] = -halfpi;
+        }
+
+        p.rotation[1] = ((p.rotation[1] % twopi) + twopi) % twopi;
+    }
+
+    updateMatrix() {
+        const degrees = this.rotation.map(x => x * 180 / Math.PI);
+        const q = quat.fromEuler(quat.create(), ...degrees);
+        mat4.fromRotationTranslationScale(
+            this.matrix,
+            q,
+            this.translation,
+            this.scale);
+    }
+
+    keydownHandler(e) {
+        this.keys[e.code] = true;
+    }
+
+    keyupHandler(e) {
+        this.keys[e.code] = false;
+    }
+
+}
